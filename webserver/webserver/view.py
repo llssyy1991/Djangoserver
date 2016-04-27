92aba1d1961fb1edc6ba899b1fc217b2939b6fd3
llssyy1991 <lisiyuan199105@gmail.com>
2016-04-27 01:12:21 -0400

    correct show_order_unfinished-16

--- webserver/webserver/view.py
+++ webserver/webserver/view.py
@@ -147,7 +147,7 @@ def change_order_status(request):
 	status = ast.literal_eval(req)
 	order_num=status["order_num"]
 	if status["status"] == "reject":
-		order_delete=db.user.update({"order.ord_n":int(order_num)},{"order.$":1})
+		order_delete=db.user.find({"order.ord_n":int(order_num)},{"order.$":1})
 		print order_delete
 		BSKY=0
 		PJNW=0
